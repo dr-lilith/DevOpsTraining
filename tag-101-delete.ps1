e6ab26e4a7c081f1dffcbfd4845cfa1d80a5b1f7
Irina <ireana.melnikova@gmail.com>
2021-09-27 22:08:08 +0300

    added 2 scripts

---
+++ tag-101-delete.ps1
@@ -0,0 +1,14 @@
+﻿#ROLL BACK 1.0.1 TAG
+cd ~
+git clone https://github.com/dr-lilith/PowerShell-training.git ./devopsHomeworkIrina > "&1"
+cd ./devopsHomeworkIrina
+
+"Deleting tag 1.0.1"
+
+git tag --delete 1.0.1 > "&1"
+git push origin --delete 1.0.1 > "&1"
+
+"Cleaning up the cloned repo."
+cd ~
+rmdir ./devopsHomeworkIrina -r -Force
+"Done"
